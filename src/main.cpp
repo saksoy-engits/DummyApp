e01ce77d010f73fc000b2a476fa0b05c1ecf0959
saksoy-engits <saksoy@engits.com>
2025-03-19 12:29:12 +0100

    Dummy App

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "MyLib/MyLib.h"
+
+int main() {
+    std::cout << "MyApp is running!" << std::endl;
+    // Call a function from MyLib
+    MyLib::say_hello();
+    return 0;
+}
